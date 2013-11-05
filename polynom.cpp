01f09e6eb3b55071c9091fda40fb1227eafeed5b
Evgenii Lezhnin <z_lezhnin@mail2000.ru>
2013-11-05 12:16:39 +0700

    polynom

--- polynom.cpp
+++ polynom.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <cmath>
+//#include <sstream>
+//#include <cmath>
 #include <stdexcept>
+#include <vector>
 using namespace std;
+void solveUneven(double *x, double *y, int n);
 int main(int argc, char **argv) {
     try {
         int k; // 0 – вычисляется сам полином, 1 – его первая производная, 2 – вторая производная
@@ -40,7 +42,9 @@ int main(int argc, char **argv) {
             getline(cin,sexpr);
             getline(cin,sexpr);
         }
-        //cerr << "n " << n << " t " << t << " a " << a << " b " << b << " s " << s << endl;
+        if (s=="u") {
+            solveUneven(x,y,n);
+        }
         delete[] x;
         delete[] y;
         delete[] xres;
@@ -50,4 +54,72 @@ int main(int argc, char **argv) {
     }
             
     return 0;
+}
+
+double findMultValue(double *x, int n, vector<int> &indexes, int k) {
+    if (k==0) {
+        return 1;
+    }
+    bool *aUsing = new bool[n];
+    for (int i=0; i<n; i++) {
+        aUsing[i] = 1;
+    }
+    for (vector<int>::iterator iter=indexes.begin(); iter!=indexes.end(); ++iter) {
+        aUsing[*iter] = 0;
+    }
+    if (k==1) {
+        double sum = 0;
+        cerr << "sum : ";
+        for (int i=0; i<n; i++) {
+            if (aUsing[i]) {
+                cerr << x[i] << ' ';
+                sum -= x[i];
+            }
+        }
+        cerr << endl;
+        delete[] aUsing;
+        return sum;
+    }
+    else {
+        int iusing = 0;
+        double sum = 0;
+        int count = 0;
+        for (int i=0; i<n-indexes.size(); i++) {
+            while (!aUsing[iusing]) ++iusing;
+            indexes.push_back(iusing);
+            sum -= x[iusing]*findMultValue(x,n,indexes,k-1);
+            ++iusing;
+            ++count;
+        }
+        indexes.erase(indexes.end()-count,indexes.end());
+        return sum;
+    }
+}
+
+void solveUneven(double *x, double *y, int n) {
+    double *l = new double[n];
+    for (int k=0; k<n; k++) {
+        l[k] = 0;
+    }
+    for (int i=0; i<n; i++) {
+        double denom = 1;
+        for (int j=0; j<n; j++) {
+            if (i!=j) {
+                denom *= x[i]-x[j];
+            }
+        }
+        double mult = y[i]/denom;
+        vector<int> indexes; //исключаемые индексы
+        indexes.push_back(i);
+        cerr << i << "mult : " << mult << " denom: " << denom << endl;
+        for (int k=0; k<n; k++) {
+            l[k] += mult*findMultValue(x,n,indexes,k);
+            cerr << "l " << k << " " << l[k] << endl;
+        }
+    }
+    for (int i=0; i<n; i++) {
+        cout << l[i] <<' ';
+    }
+    cout << endl;
+    delete[] l;
 }
